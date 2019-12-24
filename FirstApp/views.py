9d0a829890d4784b696fd95e600c58f64ef963fd
wjk <wangjunke>
2019-12-24 16:12:00 +0800

    代码更新

--- FirstApp/views.py
+++ FirstApp/views.py
@@ -20,12 +20,11 @@ def scanList(request):
 
 # 扫描列表
 def scanCommit(request):
-    print('收到资料:')
-    print(request.method)
     if (request.method == 'POST'):
         name = request.POST.get('name', None)
         arr = utils.fileListFunc(name)
         result = json.dumps(arr,ensure_ascii=False)
+
     return HttpResponse(result)
     # return render(request, 'scanList.html')
 
@@ -33,6 +32,7 @@ if __name__ == '__main__':
     s = ['asdf',123,'aaa','bbb','我是中国人']
     # ["asdf", 123, "aaa", "bbb", "\u6211\u662f\u4e2d\u56fd\u4eba"]
     p = json.dumps(s)
+
     # ["asdf", 123, "aaa", "bbb", "我是中国人"]
     p = json.dumps(s, ensure_ascii= False) #ensure_ascii=False 就不会用 ASCII 编码，中文就可以正常显示了
     print(p)
